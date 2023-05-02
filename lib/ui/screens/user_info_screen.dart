d6e8ea7c4927921d3806e20d050a73743fa6e0cd
Volkov  A <maloletka2222@mail.ru>
2023-05-02 22:25:23 +0300

    add Drawer

--- lib/ui/screens/user_info_screen.dart
+++ lib/ui/screens/user_info_screen.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/domain/service/firebase_service.dart';
 
-class UserInfoScreen extends StatelessWidget {
+class UserInfoScreen extends StatefulWidget {
   const UserInfoScreen({
     Key? key,
     required this.user,
@@ -11,14 +11,58 @@ class UserInfoScreen extends StatelessWidget {
   final User user;
 
   @override
+  State<UserInfoScreen> createState() => _UserInfoScreenState();
+}
+
+class _UserInfoScreenState extends State<UserInfoScreen> {
+  bool showUserDetails = false;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drawer Demo'),
+        automaticallyImplyLeading: false,
+      ),
+      drawer: Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new DrawerHeader(
+              margin: EdgeInsets.zero,
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader (
+                decoration: BoxDecoration(color: Colors.orange),
+                accountName: Text('User name: ${widget.user.displayName}'),
+                accountEmail:  Text('User email: ${widget.user.email}'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage('img/1.png'),
+                ),
+                onDetailsPressed: (){
+                  setState(() {
+                  showUserDetails = !showUserDetails;
+                });},
+              ),
+            ),
+            Expanded(child: Container(
+              child: showUserDetails ?
+                  TextButton(onPressed: (){}, child: Text('Change avatar'))
+                  : null,
+            )),
+             ListTile(
+                title: new Text("О себе"),
+                onTap: (){}
+            ),
+             ListTile(
+                title: new Text("Настройки"),
+                onTap: (){}
+            )
+          ],
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('User name: ${user.displayName}'),
-            Text('User email: ${user.email}'),
             StreamBuilder<User?>(
               stream: FirebaseService().auth.userChanges(),
               builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
